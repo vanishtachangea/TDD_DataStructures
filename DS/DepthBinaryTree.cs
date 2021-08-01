d17e331f78b0f03aa7d8a8c023309df70413d9f8
vanishta <vanishta.changea@gmail.com>
2021-08-01 23:00:39 +0200

    Depth Binary Tree

---
+++ DS/DepthBinaryTree.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace DS
+{
+
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public class DepthBinaryTree
+    {
+
+        public int MaxDepth(TreeNode root)
+        {
+            int cnt = 0;
+            if (root != null)
+            {
+                cnt = DFS(root, 0);
+            }
+            return cnt;
+        }
+        public int DFS(TreeNode root, int cnt)
+        {
+            if (root == null)
+            {
+                return cnt;
+            }
+            if (root != null)
+            {
+                cnt += 1;
+                return Math.Max(DFS(root.left, cnt), DFS(root.right, cnt));
+            }
+            return cnt;
+
+        }
+    }
+}
